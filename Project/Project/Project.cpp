fdfbbe0bdc7ee61ff5eb0740fbb2ff9e794107ae
Choheeseok <dswill038@gmail.com>
2021-08-09 04:51:46 +0900

    LNK2019 오류 해결

--- Project/Project/Project.cpp
+++ Project/Project/Project.cpp
@@ -1,8 +1,17 @@
-﻿#include <iostream>
-#include <fbxsdk.h>
+﻿#include "pch.h"
 
 int main()
 {
-	FbxManager* manager = nullptr;
-	std::cout << "Hello World!\n";
+	for (auto& p : directory_iterator("Fbx files/Models/")) {
+		string extension;
+		if (p.path().has_extension())
+			extension = p.path().extension().string();
+
+		if (extension == ".fbx" ||
+			extension == ".FBX") {
+		}
+	}
+
+	FbxManager* a{};
+	a->Create();
 }
